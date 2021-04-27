c2ae955eb678fc15e21b6733b5172d4afba65122
haki9975 <elias.robert.hakim@gmail.com>
2021-04-27 00:56:09 -0400

    Done for the night

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,16 @@ class SessionsController < ApplicationController
     def new
         @user = User.find_by_id(params[:id])
     end
+
+    def create
+        @user = User.find_by_username(user: params[:user][:username])
+        if @user && @user.authenticate(params[:user][:password])
+            flash[:message] = "Welcome Back!"
+            session[:user_id] = @user.id
+            redirect # landing page
+        else
+            flash[:]
+            redirect login_path
+        end
+    end
 end
