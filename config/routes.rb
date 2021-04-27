77ed445bf333337f03def7d706c2584d4a41fbbf
haki9975 <elias.robert.hakim@gmail.com>
2021-04-26 22:23:23 -0400

    add signup route, add user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :service_records
   resources :bikes
+  get '/signup', to: 'user#new'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
